38def2086bca44dd20e223a35dc20bc2cc701d0d
Pedro Muñoz <pmz94@hotmail.com>
2018-04-04 19:18:36 -0700

    .

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,26 +1,24 @@
 $(function() {
-    var producto = $('[name="product"]');
     var precio = $('[name="price"]');
     var cantidad = $('[name="quantity"]');
-    var subtotal = $('[name="subtotal"]')
-    var calculo = 0;
-    var total = $('[name="total"]');
+
+    function calcular() {
+        var subtotal = isNaN(parseInt(precio.val() * cantidad.val())) ? 0 : (precio.val() * cantidad.val());
+        var total = subtotal + 1;
+        if(precio === 0 && cantidad === 0 || subtotal === 0) {
+            subtotal = 0;
+            total = 0;
+        }
+        $('#subtotal').text(subtotal);
+        $('#total').text(total);
+    }
 
     cantidad.keyup(function() {
-        calculo = isNaN(parseInt(precio.val() * cantidad.val())) ? 0 : (precio.val() * cantidad.val());
-        if(calculo === 0) {
-            calculo = 0;
-        }
-        subtotal.text(calculo);
-        total.text(calculo + 1);
+        calcular();
+
     });
 
     precio.keyup(function() {
-        calculo = isNaN(parseInt(precio.val() * cantidad.val())) ? 0 : (precio.val() * cantidad.val());
-        if(calculo === 0) {
-            calculo = 0;
-        }
-        subtotal.text(calculo);
-        total.text(calculo + 1);
+        calcular();
     });
 });
