4f2bb9afea25b16cae29493169f3f0d8b153defe
Pedro Muñoz <pmz94@hotmail.com>
2018-08-09 23:20:43 -0700

    .

--- obtenerservicios.php
+++ obtenerservicios.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = require_once 'app/conexion.php';
+$db = include_once 'app/conexion.php';
 $query = $db->prepare('
 	SELECT *
 	FROM servicios
@@ -8,35 +8,23 @@ $query = $db->prepare('
 
 $query->execute();
 
-
 $result = $query->fetchAll(\PDO::FETCH_ASSOC);
 $data = [];
-$filtered_rows = $query->rowCount();
+$count = $query->rowCount();
 
 foreach($result as $row) {
 	$sub_array = [];
-	$sub_array[] = date_format(date_create($row['fechahora']), 'd/m/Y');
-	$sub_array[] = date_format(date_create($row['fechahora']), 'h:ia');
-	$sub_array[] = $row['correo'];
-	$sub_array[] = '$' . $row['pagoTotal'];
-	$sub_array[] = $row['idVenta'];
-	$sub_array[] = $row['estado'];
-	$sub_array[] = '<button name = "view" id = "' . $row['idTransaccion'] . '" class = "btn btn-paypal-2 btn-sm view">Ver</button>';
-
-	if($row['devuelto'] == 1) {
-		$sub_array[] = '<button class = "btn btn-paypal-2 btn-sm" disabled>Devuelto</button>';
-	} else {
-		$sub_array[] = '<button name = "refund" id = "' . $row['idVenta'] . '" class = "btn btn-paypal-2 btn-sm refund">Devolucion</button>';
-	}
+	$sub_array[] = $row['idServicio'];
+	$sub_array[] = $row['nomServicio'];
+	$sub_array[] = $row['importe'];
 
 	$data[] = $sub_array;
 }
 
 $output = [
-	'draw' => intval($_POST['draw']),
-	'recordsTotal' => $filtered_rows,
-	'recordsFiltered' => $todastransacciones,
+	'recordsTotal' => $count,
+	'recordsFiltered' => $count,
 	'data' => $data,
 ];
 
-echo json_encode($output);
+echo json_encode($output, JSON_PRETTY_PRINT);
