cd2ae4a58aa003f72d1a229499663175469c356e
Pedro Muñoz <pmz94@hotmail.com>
2021-12-18 00:06:49 -0700

    .

--- public/sql/querys.sql
+++ public/sql/querys.sql
@@ -4,9 +4,9 @@ SELECT
 	COALESCE(COUNT(t.id_producto), 0) AS frecuencia
 FROM transacciones t
 LEFT JOIN compradores c
-    ON t.clave_comprador = c.clave
+ON t.clave_comprador = c.clave
 LEFT JOIN productos p
-    ON t.id_producto = p.id
+ON t.id_producto = p.id
 WHERE c.correo = ''
 GROUP BY p.id
 ORDER BY p.id;
